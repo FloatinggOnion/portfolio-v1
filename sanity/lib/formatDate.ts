9cdc2ce43dabda491a85b7f51b4869be605798dd
floatinggonion <jesseosems123@gmail.com>
2024-12-03 18:56:09 +0100

    clean up code

--- sanity/lib/formatDate.ts
+++ sanity/lib/formatDate.ts
@@ -1,21 +1,17 @@
 const formatDate = (inputDate: string) => {
     const date = new Date(inputDate);
-    console.log(date);
     return date.toLocaleString('default', { month: 'long', year: 'numeric' });
 }
 export const formatDateLong = (inputDate: string) => {
     const date = new Date(inputDate);
-    console.log(date);
     return date.toLocaleString('default', { day: 'numeric', month: 'long', year: 'numeric' });
 }
 export const formatDateShort = (inputDate: string) => {
     const date = new Date(inputDate);
-    console.log(date);
     return date.toLocaleString('default', { day: 'numeric', month: 'short', year: 'numeric' });
 }
 export const formatTime = (inputDate: string) => {
     const date = new Date(inputDate);
-    console.log(date);
     return date.toLocaleTimeString('default', { hour: '2-digit', minute: '2-digit' });
 }
 
